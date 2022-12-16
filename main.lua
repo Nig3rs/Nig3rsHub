387eaa5117273ac04dee7f629652d5877892f2ae
Nig3rs <109903378+Nig3rs@users.noreply.github.com>
2022-12-16 21:27:02 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,7 @@
 local disc = Instance.new("Hint")
 disc.Name = "discinvite"
 disc.Text = " disc invite : .gg/HbVUmYxves "
+if game.Workspace:FindFirstChild("discinvite") then
+  return
+ end
 disc.Parent = game.workspace
