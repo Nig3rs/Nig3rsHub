00ceb0440be19169676f2082992446061336e560
Nig3rs <109903378+Nig3rs@users.noreply.github.com>
2022-12-17 11:48:41 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -114,3 +114,48 @@ local C = Y.Slider({
 	Def = 16
 })
 end
+if game.PlaceI == 7896264844 then
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+
+local X = Material.Load({
+	Title = "Nig3rsHub - Item Asylum",
+	Style = 3,
+	SizeX = 500,
+	SizeY = 350,
+	Theme = "Dark",
+	ColorOverrides = {
+		MainFrame = Color3.fromRGB(235,235,235)
+	}
+})
+
+local Main = X.New({
+	Title = "Main"
+})
+
+
+local A = Main.Button({
+	Text = "Find All Markers",
+	Callback = function()
+		print("hello")
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This can get you banned so yeah"
+			})
+		end
+	}
+})
+
+
+local walkspeed = Main.Slider({
+	Text = "Walkspeed",
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+	end,
+	Min = 16,
+	Max = 150,
+	Def = 16
+})
+
+end
